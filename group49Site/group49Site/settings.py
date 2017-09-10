00265f9de8e2ed31e553a031d72b6d16412acc53
Raj Rosello <rajrosello21@gmail.com>
2017-09-10 12:04:49 +1000

    Deleted polls app(initial app) and added introduction_page app

--- group49Site/group49Site/settings.py
+++ group49Site/group49Site/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'group49Site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'team49',
+        'HOST': '',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'team49',
+        
     }
 }
 
---
+++ group49Site/introduction_page/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IntroductionPageConfig(AppConfig):
+    name = 'introduction_page'
