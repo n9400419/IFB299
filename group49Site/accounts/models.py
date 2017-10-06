ab2363462460bb16d7e67f7329a662611a20159d
Raj Rosello <rajrosello21@gmail.com>
2017-10-06 17:47:54 +1000

    as

--- group49Site/accounts/models.py
+++ group49Site/accounts/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     email_address = models.EmailField(max_length=50)
     address = models.CharField(max_length=100)
     date_of_birth = models.DateField()
